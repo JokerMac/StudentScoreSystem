41af8243871c182c6ae3574220b901dc42641cde
huangzebin <1185202783@qq.com>
2018-04-28 17:59:59 +0800

    将header抽出为组件

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,14 @@
 export default {
-    name: "c-header",
-    data() {
-      return {};
+  name: "app-header",
+  data() {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1'
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      // console.log(key, keyPath);
     }
-  };
+  }
+};
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,6 +1,30 @@
 <template>
     <div class="header">
-        <div class="header-content">这里是头部</div>
+        <el-header class="header-content">
+            <div class="logo-wrapper">
+                <a href="" class="logo">后台管理系统BMS</a>
+            </div>
+            <el-menu :default-active="activeIndex" class="middle-area" mode="horizontal" @select="handleSelect">
+                <el-menu-item index="1">首页</el-menu-item>
+                <!-- <el-submenu index="2">
+                <template slot="title">我的工作台</template>
+                <el-menu-item index="2-1">选项1</el-menu-item>
+                <el-menu-item index="2-2">选项2</el-menu-item>
+                <el-menu-item index="2-3">选项3</el-menu-item>
+                <el-submenu index="2-4">
+                    <template slot="title">选项4</template>
+                    <el-menu-item index="2-4-1">选项1</el-menu-item>
+                    <el-menu-item index="2-4-2">选项2</el-menu-item>
+                    <el-menu-item index="2-4-3">选项3</el-menu-item>
+                </el-submenu>
+            </el-submenu> -->
+                <el-menu-item index="2">帮助中心</el-menu-item>
+                <el-menu-item index="3">
+                    关于
+                    <!-- <a href="https://www.ele.me" target="_blank">关于</a> -->
+                </el-menu-item>
+            </el-menu>
+        </el-header>
     </div>
 </template>
 
