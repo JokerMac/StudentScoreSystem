6de0c5ecc39759ba2ceb4bf4fd4b8462ca3d2107
Joker <1185202783@qq.com>
2018-05-01 22:12:39 +0800

    添加书籍列表页面。

---
+++ src/views/library/booklist/booklist.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="book-list">
+      <el-table
+    :data="tableData3"
+    style="width: 100%"
+    height="250">
+    <el-table-column
+      fixed
+      prop="date"
+      label="日期"
+      width="150">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="province"
+      label="省份"
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="city"
+      label="市区"
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址"
+      width="300">
+    </el-table-column>
+    <el-table-column
+      prop="zip"
+      label="邮编"
+      width="120">
+    </el-table-column>
+  </el-table>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import booklist from "./booklist.js";
+export default {
+  ...booklist
+};
+</script>
+
+<style src="./booklist.less"  lang="less" scoped></style>
