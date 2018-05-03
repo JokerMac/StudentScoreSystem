1af371ca8033518908378e7f17190668aaf236c9
huangzebin <1185202783@qq.com>
2018-05-03 18:24:45 +0800

    侧边栏添加跳转。

--- src/components/aside/aside.js
+++ src/components/aside/aside.js
@@ -1,3 +1,5 @@
+// import router from 'vue-router';
+
 export default {
     name: "app-aside",
     data() {
@@ -12,6 +14,13 @@ export default {
         },
         handleClose(key, keyPath) {
             console.log(key, keyPath);
+        },
+        gotoBooklist(){
+            this.$router.push({ path: `/library/booklist` });
+        },
+        gotoBorrowRecord(){
+            const userId = 123;
+            this.$router.push({ path: `/library/borrowRecord/${userId}` });
         }
     }
 };
--- src/components/aside/aside.vue
+++ src/components/aside/aside.vue
@@ -9,8 +9,8 @@
                     </template>
                     <el-menu-item-group>
                         <!-- <template slot="title">分组一</template> -->
-                        <el-menu-item index="1-1">书籍列表</el-menu-item>
-                        <el-menu-item index="1-2">借阅记录</el-menu-item>
+                        <el-menu-item index="1-1" @click="gotoBooklist()">书籍列表</el-menu-item>
+                        <el-menu-item index="1-2" @click="gotoBorrowRecord()">借阅记录</el-menu-item>
                         <el-menu-item index="1-3">添加书籍</el-menu-item>
                     </el-menu-item-group>
                     <!-- <el-menu-item-group title="分组2">
