76a4bdc628eae083961fa6385b6fe5a1fecdcba7
huangzebin <1185202783@qq.com>
2018-05-08 18:02:33 +0800

    登录页面修改。

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,10 +1,92 @@
 import header from '@/components/header/header.vue';
 export default {
-    name: 'login',
-    components: {
-        'app-header': header
+  name: 'login',
+  components: {
+    'app-header': header
+  },
+  data() {
+    // var checkAge = (rule, value, callback) => {
+    //   if (!value) {
+    //     return callback(new Error('年龄不能为空'));
+    //   }
+    //   setTimeout(() => {
+    //     if (!Number.isInteger(value)) {
+    //       callback(new Error('请输入数字值'));
+    //     } else {
+    //       if (value < 18) {
+    //         callback(new Error('必须年满18岁'));
+    //       } else {
+    //         callback();
+    //       }
+    //     }
+    //   }, 1000);
+    // };
+    var validateAccount = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'));
+      } else {
+        if (this.ruleFormModel.checkPass !== '') {
+          this.$refs.ruleFormModel.validateField('checkPass');
+        }
+        callback();
+      }
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'));
+      } else {
+        if (this.ruleFormModel.checkPass !== '') {
+          this.$refs.ruleFormModel.validateField('checkPass');
+        }
+        callback();
+      }
+    };
+    // var validatePass2 = (rule, value, callback) => {
+    //   if (value === '') {
+    //     callback(new Error('请再次输入密码'));
+    //   } else if (value !== this.ruleFormModel.pass) {
+    //     callback(new Error('两次输入密码不一致!'));
+    //   } else {
+    //     callback();
+    //   }
+    // };
+    return {
+      ruleFormModel: {
+        account: '',
+        pass: ''
+        // checkPass: '',
+        // age: ''
+      },
+      rules: {
+        account: [
+          { validator: validateAccount, trigger: 'blur' }
+        ],
+        pass: [
+          { validator: validatePass, trigger: 'blur' }
+        ]
+        // checkPass: [
+        //   { validator: validatePass2, trigger: 'blur' }
+        // ],
+        // age: [
+        //   { validator: checkAge, trigger: 'blur' }
+        // ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      debugger;
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          // console.log('error submit!!');
+          return false;
+        }
+      });
     },
-    data() {
-        return {};
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
     }
+  }
 };
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,10 +1,31 @@
 <template>
-    <div class="login">
-        <!-- <app-header></app-header> -->
-        <div class="login-pannel" style="margin-top:80px;font-size:30px;">登录</div>
+  <div class="login">
+    <!-- <app-header></app-header> -->
+    <div class="login-panel">
+      <div class="title-wrapper">登录</div>
+      <div class="content-wrapper">
+        <el-form label-position="left" :model="ruleFormModel" status-icon :rules="rules" ref="ruleFormRef" label-width="100px" class="demo-ruleForm" size="medium">
+          <el-form-item label="账号：" prop="account">
+            <el-input type="password" v-model="ruleFormModel.account" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="密码：" prop="pass">
+            <el-input type="password" v-model="ruleFormModel.pass" auto-complete="off"></el-input>
+          </el-form-item>
+          <!-- <el-form-item label="确认密码" prop="checkPass">
+            <el-input type="password" v-model="ruleFormModel.checkPass" auto-complete="off"></el-input>
+          </el-form-item> -->
+          <!-- <el-form-item label="年龄" prop="age">
+            <el-input v-model.number="ruleFormModel.age"></el-input>
+          </el-form-item> -->
+          <el-form-item class="btn-wrapper">
+            <el-button type="primary" @click="submitForm('ruleFormRef')">提交</el-button>
+            <el-button @click="resetForm('ruleFormRef')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
     </div>
+  </div>
 </template>
-
 
 <script type="text/ecmascript-6">
 import login from "./login.js";
