11e0ed5be80829e81d38d85b61ff8f964002f272
huangzebin <1185202783@qq.com>
2018-05-14 19:06:43 +0800

    统一管理保存到localStorage的数据的key值。

---
+++ src/common/js/enum.js
@@ -0,0 +1,13 @@
+export const type = {
+    LOGIN: 'login',
+    LOGOUT: 'logout'
+};
+
+export const KeyEnum = {
+    /**
+     * 保存到localStorage的数据的key。
+     */
+    TOKEN: 'token'
+};
+
+export default type;
