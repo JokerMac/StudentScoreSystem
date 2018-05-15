e897c98a5dd3c7c30c4480ee1d577db67c537e91
huangzebin <1185202783@qq.com>
2018-05-15 17:58:17 +0800

    vuex添加登入登出的状态，添加或者删除token。

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,17 +1,20 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
 import * as types from './mutation-types.js';
+import localStore from '@/common/js/local-store.js';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-    state:{},
-    mutations:{
-        [types.LOGIN](state){
-            alert('login');
+    state: {},
+    mutations: {
+        [types.LOGIN](state,data) {
+            localStore.set('token', data.token);
+            //添加vuex的state状态            
         },
-        [types.LOGOUT](state){
-
+        [types.LOGOUT](state) {
+            localStore.remove('token');
+            //添加vuex的state状态
         }
     }
 });
