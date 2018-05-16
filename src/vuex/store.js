14fd912fac148f950d8b20ba88bb5a1ca1d867f3
huangzebin <1185202783@qq.com>
2018-05-16 18:01:37 +0800

    保存到本地的数据的key使用常量。

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,19 +1,21 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
 import * as types from './mutation-types.js';
+import * as localStoreKey from '@/common/js/local-store-key.js';
 import localStore from '@/common/js/local-store.js';
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store({
+const store = new Vuex.Store({  
     state: {},
     mutations: {
-        [types.LOGIN](state,data) {
-            localStore.set('token', data.token);
+        [types.LOGIN](state, data) {
+            // debugger;
+            localStore.set(localStoreKey.LOGIN_INFO, data);
             //添加vuex的state状态            
         },
         [types.LOGOUT](state) {
-            localStore.remove('token');
+            localStore.remove(localStoreKey.LOGIN_INFO);
             //添加vuex的state状态
         }
     }
