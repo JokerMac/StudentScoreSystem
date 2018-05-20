771194e9fcc46914bc176d640604770fefc9e287
Joker <1185202783@qq.com>
2018-05-20 23:17:44 +0800

    1、侧边栏菜单项添加id属性。2、通过mouted和watch解决菜单栏选中项错误的问题。3、通过获取id解决浏览器后退时焦点错误的问题。

--- src/components/aside/aside.js
+++ src/components/aside/aside.js
@@ -5,10 +5,10 @@ export default {
     data() {
         return {
             activeIndex: '1-1',
-            userId :'1234',
-            menuObj:{
-                'booklist':'1-1',
-                'borrowRecord':'1-2'
+            userId: '1234',
+            menuObj: {
+                'booklist': '1-1',
+                'borrowRecord': '1-2'
             }
         };
     },
@@ -19,15 +19,28 @@ export default {
         handleClose(key, keyPath) {
             console.log(key, keyPath);
         },
-        gotoBooklist(){
+        gotoBooklist() {
             this.$router.push({ name: 'booklist' });
         },
-        gotoBorrowRecord(){
+        gotoBorrowRecord() {
             // this.$router.push({ path: `library/borrowRecord/${userId}` });
-            this.$router.push({ name: 'borrowRecord',params:{userId:this.userId} });
+            this.$router.push({ name: 'borrowRecord', params: { userId: this.userId } });
+        },
+        updateActiveMenuItem() {//修复经过刷新或者回退以后，头部导航栏的选中项与实际页面不匹配的问题。
+            this.activeIndex = this.menuObj[this.$route.name];
+            //解决点击浏览器后退按钮时，菜单栏选中项焦点没有改变导致背景颜色错误的问题。
+            document.getElementById(this.activeIndex).focus();
         }
     },
-    mounted(){//dom挂载完成，解决刷新页面以后，导航栏的选中项错误的问题。
-        this.activeIndex=this.menuObj[this.$route.name];
+    mounted() {//dom挂载完成，解决【刷新页面】以后，导航栏的选中项错误的问题。
+        this.updateActiveMenuItem();
+    },
+    watch: {
+        //刷新页面的时候，不会进入watch，所以需要用mounted解决刷新时菜单项选中错误问题。
+        //此处用updated不起作用，因为侧边栏组件没有因为地址栏变化而update，只有内容区域有变化。所以只能
+        //改用watch监测地址变化来解决选中项错误的问题。
+        '$route.name': function (newValue, oldValue) {
+            this.updateActiveMenuItem();
+        }
     }
 };
