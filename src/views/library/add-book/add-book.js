32126047fd6c28e95d0473ea7d260bc7281eb191
Joker <1185202783@qq.com>
2018-05-25 14:25:14 +0800

    添加书籍界面增加el-dialog弹窗，提供填写书籍信息的表单。

--- src/views/library/add-book/add-book.js
+++ src/views/library/add-book/add-book.js
@@ -1,18 +1,24 @@
 // import {getPhotos} from '@/api/library.js';
+import { MessageBox } from 'element-ui';
 
 export default {
     name: "add-book",
     data() {
         return {
+            dialogFormVisible: false,
+            form: {
+                name: '',
+                date: ''
+            },
+            formLabelWidth: '120px'
         };
     },
     methods: {
+        showDialog: function (showDialogFlag, createBookFlag) {
+            this.dialogFormVisible = showDialogFlag;
+            if (createBookFlag) {
+                alert('创建书籍名称：' + this.form.name + ' 日期：' + this.form.date);
+            }
+        }
     }
-    // created(){
-    //     const cur=this;
-    //     getPhotos()
-    //     .then(function(data){
-    //         cur.picList=data.slice(0,10);
-    //     });
-    // }
 };
--- src/views/library/add-book/add-book.vue
+++ src/views/library/add-book/add-book.vue
@@ -1,6 +1,20 @@
 <template>
     <div class="add-book">
-        添加书籍
+        <el-button type="text" @click="showDialog(true)">添加书籍</el-button>
+        <el-dialog title="添加书籍" :visible.sync="dialogFormVisible">
+            <el-form :model="form">
+                <el-form-item label="书籍名称" :label-width="formLabelWidth">
+                    <el-input v-model="form.name" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="录入日期" :label-width="formLabelWidth">
+                    <el-input v-model="form.date" auto-complete="off" placeholder="格式：2018-05-24"></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="showDialog(false)">取 消</el-button>
+                <el-button type="primary" @click="showDialog(false,true)">确 定</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
